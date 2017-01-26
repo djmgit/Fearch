56bfd8f5201ee47059517e5e76c71a2cd3a42dee
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2017-01-26 17:32:08 +0530

    added navigation

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+	
+	$(".welcome").click(function(e) {
+		e.preventDefault();
+		$('html,body').animate({
+        scrollTop: $("#welcome").offset().top},
+        'slow');
+	});
+
+	$(".howto").click(function(e) {
+		e.preventDefault();
+		$('html,body').animate({
+        scrollTop: $("#howto").offset().top},
+        'slow');
+
+	});
+
+	$(".getfearch").click(function(e) {
+		e.preventDefault();
+		$('html,body').animate({
+        scrollTop: $("#getfearch").offset().top},
+        'slow');
+	});
+
+	$(".getinvolved").click(function(e) {
+		e.preventDefault();
+		$('html,body').animate({
+        scrollTop: $("#getinvolved").offset().top},
+        'slow');
+	});
+	$(".goto-howto").click(function(e) {
+		e.preventDefault();
+		$('html,body').animate({
+        scrollTop: $("#howto").offset().top},
+        'slow');
+	});
+	$(".goto-get").click(function(e) {
+		e.preventDefault();
+		$('html,body').animate({
+        scrollTop: $("#getfearch").offset().top},
+        'slow');
+	});
+	$(".goto-involved").click(function(e) {
+		e.preventDefault();
+		$('html,body').animate({
+        scrollTop: $("#getinvolved").offset().top},
+        'slow');
+	});
+
+
+});
